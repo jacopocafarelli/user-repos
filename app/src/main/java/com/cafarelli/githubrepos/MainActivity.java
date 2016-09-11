ea40f42afdec2646d83fbb3b259790336dde18a6
jacopocafarelli <jacopo.cafarelli@gmail.com>
2016-09-11 12:18:35 +0200

    Fix style for MainActivity

--- app/src/main/java/com/cafarelli/githubrepos/MainActivity.java
+++ app/src/main/java/com/cafarelli/githubrepos/MainActivity.java
@@ -1,8 +1,6 @@
 package com.cafarelli.githubrepos;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,8 +20,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ((MyApplication) getApplication()).getNetworkComponent().inject(this);
 
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 
